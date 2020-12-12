8313d19f74ff235fc83471edbe902c16d1dfe678
Ester Molinari <estermolinari135@gmail.com>
2020-12-12 11:50:32 +0000

    Aggiunta lista di char

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
 	// ----- Creazione oggetto Pila ----- //
   Pila<int> pila1;
+	Pila<char> pila2;
 
 	// ----- Test pilaVuota ----- //
 	cout << "Controllo elementi pila..." << endl;
@@ -34,5 +35,34 @@ int main() {
 	// ----- Test stampPila ----- //
   pila1.stampaPila(pila1);
 
+	// ------------------------- TEST PILA DI CHAR --------------- //
+
+	// ----- Test pilaVuota ----- //
+	cout << "Controllo elementi pila..." << endl;
+  if ( pila2.pilaVuota() ) {
+		cout << "> La pila è vuota" << endl;
+	}
+	
+	// ----- Test inPila ----- //
+  pila2.inPila('a');
+  pila2.inPila('b');
+  
+	// ----- Test pilaVuota ----- //
+	cout << "Controllo elementi pila..." << endl;
+	if ( !pila2.pilaVuota() ) {
+		cout << "> C'è almeno un elemento nella pila" << endl;
+	}
+	
+	// ----- Test fuoriPila (pop) ----- //
+  pila2.fuoriPila();
+  cout << "Valore elemento in testa: " << pila2.leggiPila() << endl;
+  
+	// ----- Test inPila ----- //
+  pila2.inPila('v');
+  pila2.inPila('z');
+
+	// ----- Test stampPila ----- //
+  pila2.stampaPila(pila2);
+	
 	return 0;
 }
